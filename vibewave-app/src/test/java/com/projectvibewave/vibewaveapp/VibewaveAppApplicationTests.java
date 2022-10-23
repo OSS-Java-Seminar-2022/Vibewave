7873a2a717ced3019a6409d3f3d7768dc285250d
Toni Kazinoti <tk48322@oss.unist.hr>
2022-10-23 09:40:24 +0200

    project setup done

---
+++ vibewave-app/src/test/java/com/projectvibewave/vibewaveapp/VibewaveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectvibewave.vibewaveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibewaveAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
