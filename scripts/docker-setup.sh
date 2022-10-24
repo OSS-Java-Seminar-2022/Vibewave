972e60b2f55ca23eb3035977ba77c0e53a7102cc
banquetblintzs <dev@example.com>
2022-10-24 14:19:08 +0200

    helper script for docker setup

---
+++ scripts/docker-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# note: needs to be run with sudo 
+
+# create database
+# docker pull postgres
+# docker run --name vibewavedb -d -p 5432:5432 -e POSTGRES_PASSWORD=postgres postgres
+# sudo docker exec -it --user postgres vibewavedb sh -c "psql -c 'CREATE DATABASE vibewave OWNER postgres;'"
+
+# create network
+# docker network create vibewave-net
+# docker network connect vibewave-net vibewavedb
+
+# build project
+# ./mvnw install # note: path needs to be ammended to correct one
+
+# build app container
+# docker build -t vibewave-app ../vibewave-app/
+
+# run app with docker
+# docker run -p 9090:8080 --name vibewave-app --net vibewave-net -e DB_HOSTNAME='vibewavedb' vibewave-app
