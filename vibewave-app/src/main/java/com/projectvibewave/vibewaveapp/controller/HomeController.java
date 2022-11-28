8d43191d9ce864fcc2fbdd8c58d943154bbbfbbe
tonikazinoti <tk48322@oss.unist.hr>
2022-11-28 15:42:30 +0100

    users can now edit their settings, lots of other changes, layout system

--- vibewave-app/src/main/java/com/projectvibewave/vibewaveapp/controller/HomeController.java
+++ vibewave-app/src/main/java/com/projectvibewave/vibewaveapp/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.projectvibewave.vibewaveapp.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,8 +11,9 @@ public class HomeController {
     private final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping("/")
-    public String homeView() {
+    public String homeView(Model model) {
         logger.info("Accessed Home Page");
+        model.addAttribute("PageName", "Home");
         return "home";
     }
 }
---
+++ vibewave-app/src/main/java/com/projectvibewave/vibewaveapp/service/FileServiceImpl.java
@@ -0,0 +1,78 @@
+package com.projectvibewave.vibewaveapp.service;
+
+import com.projectvibewave.vibewaveapp.controller.AuthController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+@Service
+public class FileServiceImpl implements FileService{
+    private final Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
+    private final Path root = Paths.get("uploads");
+    private final String dot = ".";
+
+    @Override
+    public void init() {
+        try {
+            Files.createDirectories(root);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize folder for upload!");
+        }
+    }
+
+    @Override
+    public String save(MultipartFile file) {
+        var fileExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+        var uuid = UUID.randomUUID().toString();
+        var filename = uuid + dot + fileExtension;
+        var fullPath = this.root.resolve(filename);
+        try {
+            Files.copy(file.getInputStream(), fullPath);
+        } catch (Exception e) {
+            if (e instanceof FileAlreadyExistsException) {
+                throw new RuntimeException("A file with that name already exists.");
+            }
+
+            logger.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+
+        return filename;
+    }
+
+    @Override
+    public Resource load(String filename) {
+        try {
+            Path file = root.resolve(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Could not read the file!");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Error: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(root.toFile());
+    }
+}
